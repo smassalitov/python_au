f7e4e51ba8842922b2b670bf80a43283a17eb3c0
smassalitov <71551691+smassalitov@users.noreply.github.com>
2020-10-01 19:42:17 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,8 +1,22 @@
 fin = open("source_leetcode_data.txt")
-fout = open("intervals.md", "a+")
+fout = open("linked-list.md", "a+")
 with open('source_leetcode_data.txt', 'r') as fin:
     all_lines = fin.read().splitlines()
-print('##' + all_lines[0], file = fout)
+
+
+name = all_lines[0]
+name_list = list(name)
+name_list[0] = ''
+i = 1
+while name_list[i-1] != '.':
+    name_list[i-1] = ''
+    i+=1
+dot_place = name.find('.')
+name_list[dot_place] = ''
+name.strip()
+name = ''.join(name_list)
+
+print('##' + str(name), file = fout)
 print(all_lines[1], file = fout)
 print('```python', file = fout)
 for i in range(2, len(all_lines)):
