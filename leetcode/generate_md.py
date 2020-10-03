aaf9e816c586bd0f8ef8fa675d9847fff889f81d
smassalitov <smassalitov@gmail.com>
2020-10-03 18:42:39 +0300

    КОДСТАЙЛ

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -3,14 +3,14 @@ def GetName(all_lines):
     name_list = list(name)
     name_list[0] = ''
     i = 1
-    while name_list[i-1] != '.':
-        name_list[i-1] = ''
-        i+=1
+    while name_list[i - 1] != '.':
+        name_list[i - 1] = ''
+        i += 1
     dot_place = name.find('.')
     name_list[dot_place] = ''
     name.strip()
     name = ''.join(name_list)
-    name1 =  "-".join(name.split())
+    name1 = "-".join(name.split())
     return [name, name1]
 
 
@@ -32,14 +32,14 @@ def PrintMarkDown(all_lines):
             break
     for i in all_linked_lines:
         print(i, file=fout)
-    print('## ' + str(GetName(all_lines)[0]), file = fout)
-    print(GetUrl(all_lines), file = fout)
+    print('## ' + str(GetName(all_lines)[0]), file=fout)
+    print(GetUrl(all_lines), file=fout)
     GetCode(all_lines)
-    
-    
+
+
 with open("source_leetcode_data.txt", 'r') as fin:
     all_lines = fin.read().splitlines()
 with open('linked-list.md', 'r') as fin:
     all_linked_lines = fin.read().splitlines()
-fout = open("linked-list.md", "w")    
+fout = open("linked-list.md", "w")
 PrintMarkDown(all_lines)
