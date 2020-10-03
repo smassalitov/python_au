ec74c792a9f814e6d19fb28611523540c5c14b43
smassalitov <71551691+smassalitov@users.noreply.github.com>
2020-10-03 16:42:37 +0300

    Update generate_md.py

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -4,32 +4,36 @@ with open('linked-list.md', 'r') as fin:
     all_linked_lines = fin.read().splitlines()
 fout = open("linked-list.md", "w")
 
-name = all_lines[0]
-name_list = list(name)
-name_list[0] = ''
-i = 1
 
-while name_list[i-1] != '.':
-    name_list[i-1] = ''
-    i+=1
-dot_place = name.find('.')
-name_list[dot_place] = ''
-name.strip()
-name = ''.join(name_list)
-
-for i in range(len(all_linked_lines)):
-    if all_linked_lines[i] == '':
-        all_linked_lines.insert(i, '+ [{}](#{})'.format(name, name))
-        break
-
-for i in all_linked_lines:
-    print(i, file=fout)
-
-print('## ' + str(name), file = fout)
-print(all_lines[1], file = fout)
-print('```python', file = fout)
-for i in range(2, len(all_lines)):
-    print(all_lines[i], file = fout)
-print('```', file = fout)
-
-fout.close()
+def GetName(all_lines):
+    name = all_lines[0]
+    name_list = list(name)
+    name_list[0] = ''
+    i = 1
+    while name_list[i-1] != '.':
+        name_list[i-1] = ''
+        i+=1
+    dot_place = name.find('.')
+    name_list[dot_place] = ''
+    name.strip()
+    name = ''.join(name_list)
+    name1 =  "-".join(name.split())
+    return [name, name1]
+def GetUrl(all_lines):
+    return all_lines[1]
+def GetCode(all_lines):
+    print('```python', file=fout)
+    for i in range(2, len(all_lines)):
+        print(all_lines[i], file=fout)
+    print('```', file=fout)
+def PrintMarkDown(all_lines):
+    for i in range(len(all_linked_lines)):
+        if all_linked_lines[i] == '':
+            all_linked_lines.insert(i, '+ [{}](#{})'.format(GetName(all_lines)[0], GetName(all_lines)[1]))
+            break
+    for i in all_linked_lines:
+        print(i, file=fout)
+    print('## ' + str(GetName(all_lines)[0]), file = fout)
+    print(GetUrl(all_lines), file = fout)
+    GetCode(all_lines)
+PrintMarkDown(all_lines)
